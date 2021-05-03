2f2f70e3f85a3a3ddf437e00c385b1c0cbc72b29
ahnafabir <ahnafabir313@gmail.com>
2021-05-03 06:23:12 +0600

    project hover animation updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,18 +14,18 @@ const Projects = () => {
             <div key={idx} className="single_projects">
               <div className="project_img_container">
                 <img src={project.imgSrc} alt={project.name} className="project_img" />
+                <div className="project_btn_container">
+                  <a href={project.live} target="blank" className="project_btn">
+                    <FaRegEye style={{ fontSize: '24px' }} /> Watch Live
+                </a>
+                  <a href={project.github} target="blank" className="project_btn">
+                    <FaCode style={{ fontSize: '24px' }} /> Review Code
+                </a>
+                </div>
               </div>
               <div className="project_des">
                 <h4 className="text-center text-white">{project.name}</h4>
                 <h6 className="text-center" style={{ color: 'orange'}}>{project.language}</h6>
-              </div>
-              <div className="project_btn_container">
-                <a href={project.live} target="blank" className="project_btn">
-                  <FaRegEye style={{fontSize: '24px'}}/> Watch Live
-                </a>
-                <a href={project.github} target="blank" className="project_btn">
-                  <FaCode style={{ fontSize: '24px' }}/> Review Code
-                </a>
               </div>
             </div>
           )
